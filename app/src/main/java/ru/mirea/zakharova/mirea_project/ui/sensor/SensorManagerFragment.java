016abc7fb101f5c4d1bc485995fe92a4434a269b
donteavesdrop <import_math@mail.ru>
2023-06-04 13:28:16 +0300

    init

---
+++ app/src/main/java/ru/mirea/zakharova/mirea_project/ui/sensor/SensorManagerFragment.java
@@ -0,0 +1,54 @@
+package ru.mirea.zakharova.mirea_project.ui.sensor;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import ru.mirea.zakharova.mirea_project.databinding.FragmentSensorManagerBinding;
+
+public class SensorManagerFragment extends Fragment {
+
+    private FragmentSensorManagerBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentSensorManagerBinding.inflate(inflater, container, false);
+        View rootView = binding.getRoot();
+        ListView listView = binding.listView;
+
+        android.hardware.SensorManager sensorManager = (android.hardware.SensorManager) requireContext().getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        ArrayList<HashMap<String, Object>> arrayList = new ArrayList<>();
+        for (int i = 0; i < sensors.size(); i++) {
+            HashMap<String, Object> sensorTypeList = new HashMap<>();
+            sensorTypeList.put("Name", sensors.get(i).getName());
+            sensorTypeList.put("Value", sensors.get(i).getMaximumRange());
+            arrayList.add(sensorTypeList);
+        }
+
+        SimpleAdapter mHistory = new SimpleAdapter(requireContext(), arrayList, android.R.layout.simple_list_item_2,
+                new String[]{"Name", "Value"},
+                new int[]{android.R.id.text1, android.R.id.text2});
+        listView.setAdapter(mHistory);
+
+        return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
